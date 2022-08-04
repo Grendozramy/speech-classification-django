80f8120a126dbc18a4109afa8583c68ada0ee309
Grendozramy <zramyg@gmail.com>
2022-08-04 07:29:00 +0700

    Workplace Network - CRUD Controller

--- workplace_user_network/urls.py
+++ workplace_user_network/urls.py
@@ -6,4 +6,10 @@ app_name = 'workplace_user_network'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('workplace_network/create', views.create, name='create'),
+    path('workplace_network/store/', views.store, name='store'),
+    path('workplace_network/<int:id>/detail/', views.detail, name='detail'),
+    path('workplace_network/<int:id>/edit/', views.edit, name='edit'),
+    path('workplace_network/<int:id>/update/', views.update, name='update'),
+    path('workplace_network/<int:id>/delete/', views.delete, name='delete'),
 ]
--- workplace_user_network/views.py
+++ workplace_user_network/views.py
@@ -1,6 +1,7 @@
-from __future__ import division
-from .models import Division, Interaction, WorkplaceUserNetwork
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import Department, Division, Interaction, User, WorkplaceUserNetwork
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -8,9 +9,91 @@ def index(request):
     division_list = Division.objects.all()
     interaction_list = Interaction.objects.all()
     context = {
-        'workplace_user_network': workplaceusernetwork_list,
-        'division': division_list,
-        'interaction': interaction_list
+        'workplace_network': workplaceusernetwork_list,
+        'divisions': division_list,
+        'interactions': interaction_list
         }
 
     return render(request, 'workplace_user_network/index.html', context)
+
+def create(request):
+    workplaceusernetwork_list = WorkplaceUserNetwork.objects.all()
+    user_list = User.objects.all()
+    division_list = Division.objects.all()
+    interaction_list = Interaction.objects.all()
+    department_list = Department.objects.all()
+    context = {
+        'workplace_network': workplaceusernetwork_list,
+        'users': user_list,
+        'divisions': division_list,
+        'interactions': interaction_list,
+        'departments': department_list,
+        }
+    return render(request, 'workplace_user_network/workplace_network/create.html', context)
+
+def store(request):
+    workplacenetwork = WorkplaceUserNetwork.objects.create(
+        from_user_id = request.POST['from_user_id'],
+        to_user_id  = request.POST['to_user_id'],
+        posting_division_id  = request.POST['posting_division_id'],
+        comment_division_id  = request.POST['comment_division_id'],
+        interaction_id  = request.POST['interaction_id'],
+        posting_department_id  = request.POST['posting_department_id'],
+        comment_department_id = request.POST['comment_department_id'],
+        amount = request.POST['amount'])
+    workplacenetwork.save()
+
+    return HttpResponseRedirect(reverse('workplace_user_network:index'))
+
+def detail(request, id):
+    workplaceusernetwork_list = get_object_or_404(WorkplaceUserNetwork, id=id)
+    user_list = User.objects.all()
+    division_list = Division.objects.all()
+    interaction_list = Interaction.objects.all()
+    department_list = Department.objects.all()
+    context = {
+        'workplace_network': workplaceusernetwork_list,
+        'users': user_list,
+        'divisions': division_list,
+        'interactions': interaction_list,
+        'departments': department_list,
+    }
+    
+    return render(request, 'workplace_user_network/workplace_network/detail.html', context)
+
+def edit(request, id):
+    workplaceusernetwork_list = get_object_or_404(WorkplaceUserNetwork, id=id)
+    user_list = User.objects.all()
+    division_list = Division.objects.all()
+    interaction_list = Interaction.objects.all()
+    department_list = Department.objects.all()
+    context = {
+        'workplace_network': workplaceusernetwork_list,
+        'users': user_list,
+        'divisions': division_list,
+        'interactions': interaction_list,
+        'departments': department_list,
+    }
+    
+    return render(request, 'workplace_user_network/workplace_network/edit.html', context)
+
+
+def update(request, id):
+    workplaceusernetwork_list = get_object_or_404(WorkplaceUserNetwork, id=id)
+    workplaceusernetwork_list.from_user_id = User.objects.get(id=request.POST['from_user_id'])
+    workplaceusernetwork_list.to_user_id = User.objects.get(id=request.POST['to_user_id'])
+    workplaceusernetwork_list.posting_division_id = Division.objects.get(id=request.POST['posting_division_id'])
+    workplaceusernetwork_list.posting_department_id = Department.objects.get(id=request.POST['comment_division_id'])
+    workplaceusernetwork_list.comment_division_id = Division.objects.get(id=request.POST['posting_department_id'])
+    workplaceusernetwork_list.comment_department_id = Department.objects.get(id=request.POST['comment_department_id'])
+    workplaceusernetwork_list.interaction_id  = Interaction.objects.get(id=request.POST['interaction_id'])
+    workplaceusernetwork_list.amount = request.POST['amount']
+    workplaceusernetwork_list.save()
+
+    return HttpResponseRedirect(reverse('workplace_user_network:index'))
+
+def delete(request, id):
+    message = get_object_or_404(WorkplaceUserNetwork, id=id)
+    message.delete()
+    
+    return HttpResponseRedirect(reverse('workplace_user_network:index'))
